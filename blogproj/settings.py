2a61b4626f676f9e408a5d89dcba8244e379cb4a
LautaroLuna <nicolaslautaroluna@gmail.com>
2025-05-02 18:26:24 -0300

    Actualizo estilos, añado texto en About y mejoras en botones, boton de logout funcional

--- blogproj/settings.py
+++ blogproj/settings.py
@@ -128,6 +128,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+LOGOUT_REDIRECT_URL = 'blog-home'
+
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
